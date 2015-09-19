de98913a0c9d0b46f795ccc22b8290ac3b062ed6
apolong <apolong.lap@gmail.com>
2015-09-19 18:05:12 +0700

    Upload Demo Project:
    Demo App 15092015v4
    
    Signed-off-by: apolong <apolong.lap@gmail.com>

---
+++ Demo Code/Demo App 15092015v4/Demo App 15092015v4/Views/Maps.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Demo_App_15092015v4
+{
+    public partial class Maps : PhoneApplicationPage
+    {
+        public Maps()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public enum GoogleTileTypes
+    {
+        street
+    }
+
+    public class GoogleTile : Microsoft.Phone.Controls.Maps.TileSource
+    {
+        private GoogleTileTypes _tiletypes;
+
+        public GoogleTileTypes TileTypes
+        {
+            get { return _tiletypes; }
+            set { _tiletypes = value; }
+        }
+
+        public GoogleTile()
+        {
+            UriFormat = @"http://mt{0}.google.com/vt/lyrs={1}&z={2}&x={3}&y={4}";
+        }
+
+        public override Uri GetUri(int x, int y, int zoomLevel)
+        {
+            if (zoomLevel > 0)
+            {
+                var Url = string.Format(UriFormat, 0, "m", zoomLevel, x, y);
+                return new Uri(Url);
+            }
+            return null;
+        }
+    }
+}
---
+++ Demo Code/Demo App 15092015v4/Demo App 15092015v4/Views/TutorialPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Demo_App_15092015v4
+{
+    public partial class tutPage01 : PhoneApplicationPage
+    {
+        public tutPage01()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Views/FunctionPage.xaml", UriKind.Relative));
+        }
+    }
+}
