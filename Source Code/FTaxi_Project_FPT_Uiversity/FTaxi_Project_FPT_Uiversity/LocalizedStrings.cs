afff53a8bd5c92733fa915529946a9127cd51364
apolong <apolong.lap@gmail.com>
2015-10-06 21:34:49 +0700

    LapNV Create

---
+++ Source Code/FTaxi_Project_FPT_Uiversity/FTaxi_Project_FPT_Uiversity/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using FTaxi_Project_FPT_Uiversity.Resources;
+
+namespace FTaxi_Project_FPT_Uiversity
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
