c44623d5f196c920661dafa0d4670373ccecabf2
huuphamlc <huuphamlc@gmail.com>
2015-10-09 23:16:52 +0700

    update rider

---
+++ Source Code/FTaxi_Project_FPT_Uiversity/FT-Rider/Pages/RiderLostPassword.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace FT_Rider.Pages
+{
+    public partial class RiderLostPassword : PhoneApplicationPage
+    {
+        public RiderLostPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void txtEmail_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            txt_Email.Text = String.Empty;
+        }
+    }
+}
