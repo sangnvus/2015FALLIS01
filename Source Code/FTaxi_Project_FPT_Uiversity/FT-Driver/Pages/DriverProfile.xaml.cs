581d4e8bd58774d516ccf0c426d105839274216e
huuphamlc <huuphamlc@gmail.com>
2015-10-09 23:15:58 +0700

    update driver

---
+++ Source Code/FTaxi_Project_FPT_Uiversity/FT-Driver/Pages/DriverProfile.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace FT_Driver.Pages
+{
+    public partial class DriverProfile : PhoneApplicationPage
+    {
+        public DriverProfile()
+        {
+            InitializeComponent();
+        }
+
+        private void tbl_Tap_ChangePassword(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pages/DriverChangePassword.xaml", UriKind.Relative));
+        }
+
+        private void tbn__Tap_AddWorkPlace(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pages/DriverAddWorkPlace.xaml", UriKind.Relative));
+        }
+
+        private void tbn__Tap_AddHomePlace(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pages/DriverAddHomePlace.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ Source Code/FTaxi_Project_FPT_Uiversity/FT-Driver/Pages/Login.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.IO.IsolatedStorage;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using FT_Driver.Resources;
+
+namespace FT_Driver.Pages
+{
+    public partial class Login : PhoneApplicationPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void tbn_Tap_Login(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pages/DriverProfile.xaml", UriKind.Relative));
+        }
+
+       
+
+      
+       
+        
+    }
+}
