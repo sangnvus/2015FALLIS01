984d52167f4e210e6a3de1ffbdf0c4aaf1282f55
apolong <apolong.lap@gmail.com>
2015-10-13 01:48:55 +0700

    LapNV - Update Tut Page - DONE!
    Signed-off-by: apolong <apolong.lap@gmail.com>

--- Source Code/FTaxi_Project_FPT_Uiversity/FT-Rider/Pages/FisrtRunningAppIntro.xaml.cs
+++ Source Code/FTaxi_Project_FPT_Uiversity/FT-Rider/Pages/FisrtRunningAppIntro.xaml.cs
@@ -14,46 +14,52 @@ namespace FT_Rider.Pages
 {
     public partial class FisrtRunningAppIntro : PhoneApplicationPage
     {
-        
+        //varible
+        SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+        PivotItem pivot = null;
+
 
         public FisrtRunningAppIntro()
         {
             InitializeComponent();
-
-            //TUT page
-            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
-            mySolidColorBrush.Color = Color.FromArgb(255, 46, 159, 255);
-
-            if(pvi_Tut1.IsEnabled)
-            {
-                el_Page1.Fill = mySolidColorBrush;
-                el_Page2.Fill = el_Page3.Fill = el_Page4.Fill = el_Page5.Fill = new SolidColorBrush(Colors.White);
-            }
-            if (pvi_Tut2.IsEnabled)
-            {
-                el_Page2.Fill = mySolidColorBrush;
-                el_Page1.Fill = el_Page3.Fill = el_Page4.Fill = el_Page5.Fill = new SolidColorBrush(Colors.White);
-            }
-            if (pvi_Tut3.IsEnabled)
-            {
-                el_Page3.Fill = mySolidColorBrush;
-                el_Page1.Fill = el_Page2.Fill = el_Page4.Fill = el_Page5.Fill = new SolidColorBrush(Colors.White);
-            }
-            if (pvi_Tut4.IsEnabled)
-            {
-                el_Page4.Fill = mySolidColorBrush;
-                el_Page1.Fill = el_Page2.Fill = el_Page3.Fill = el_Page5.Fill = new SolidColorBrush(Colors.White);
-            }
-            if (pvi_Tut5.IsEnabled)
-            {
-                el_Page5.Fill = mySolidColorBrush;
-                el_Page1.Fill = el_Page2.Fill = el_Page3.Fill = el_Page4.Fill = new SolidColorBrush(Colors.White);
-            }
         }
 
         private void btn_Go_Tap(object sender, System.Windows.Input.GestureEventArgs e)
         {
             NavigationService.Navigate(new Uri("/Pages/Login.xaml", UriKind.Relative));
         }
+
+        private void pvi_Tut_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //TUT page            
+            mySolidColorBrush.Color = Color.FromArgb(255, 46, 159, 255);
+            pivot = (PivotItem)(sender as Pivot).SelectedItem;
+
+            switch (pivot.Name.ToString())
+            {
+                case "pvi_Tut1":
+                    el_Page1.Fill = mySolidColorBrush;
+                    el_Page2.Fill = el_Page3.Fill = el_Page4.Fill = el_Page5.Fill = new SolidColorBrush(Colors.White);
+                    break;
+                case "pvi_Tut2":
+                    el_Page2.Fill = mySolidColorBrush;
+                    el_Page1.Fill = el_Page3.Fill = el_Page4.Fill = el_Page5.Fill = new SolidColorBrush(Colors.White);
+                    break;
+                case "pvi_Tut3":
+                    el_Page3.Fill = mySolidColorBrush;
+                    el_Page1.Fill = el_Page2.Fill = el_Page4.Fill = el_Page5.Fill = new SolidColorBrush(Colors.White);
+                    break;
+                case "pvi_Tut4":
+                    el_Page4.Fill = mySolidColorBrush;
+                    el_Page1.Fill = el_Page2.Fill = el_Page3.Fill = el_Page5.Fill = new SolidColorBrush(Colors.White);
+                    break;
+                case "pvi_Tut5":
+                    el_Page5.Fill = mySolidColorBrush;
+                    el_Page1.Fill = el_Page2.Fill = el_Page3.Fill = el_Page4.Fill = new SolidColorBrush(Colors.White);
+                    break;
+
+            }
+        }
+
     }
 }
