e0941f8e88ebcee46773f15891a40d7b91f2f8e8
huongtt <huongttse02900@fpt.edu.vn>
2015-10-30 21:51:39 +0700

    HuongTT

--- Source Code/FTaxi_Project_FPT_Uiversity/FT-Driver/Pages/DriverProfile.xaml.cs
+++ Source Code/FTaxi_Project_FPT_Uiversity/FT-Driver/Pages/DriverProfile.xaml.cs
@@ -7,29 +7,120 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.IO.IsolatedStorage;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using FT_Driver.Classes;
+
+
 
 namespace FT_Driver.Pages
 {
-    public partial class DriverProfile : PhoneApplicationPage
+    public partial class RiderProfile : PhoneApplicationPage
+
     {
-        public DriverProfile()
+         
+        IsolatedStorageFile ISOFile = IsolatedStorageFile.GetUserStoreForApplication();
+        UserData ObjUserData = new UserData();
+
+        String[] Languages = { "Tiếng Việt", " Tiếng Anh" };
+        public RiderProfile()
         {
+            
+            
             InitializeComponent();
+            this.lpk_Language.ItemsSource = Languages;
+          
+
+            this.Loaded += RiderProfile_Loaded;
         }
+
+        private void RiderProfile_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (ISOFile.FileExists("CurrentLoginUserDetails"))//read current user login details    
+            {
+                using (IsolatedStorageFileStream fileStream = ISOFile.OpenFile("CurrentLoginUserDetails", FileMode.Open))
+                {
+                    DataContractSerializer serializer = new DataContractSerializer(typeof(UserData));
+                    ObjUserData = (UserData)serializer.ReadObject(fileStream);
+
+                }
+                StckUserDetailsUI.DataContext = ObjUserData;  
+            }  
+        }
+
+
+        
+
+        private void btn__Tap_Logout(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            LogOut();
+        }
+
+        public void LogOut()
+        {
+            CustomMessageBox messageBox = new CustomMessageBox()
+            {
+                //set the properties
+                Caption = "Thông báo",
+                Message = "Bạn có muốn thoát không?",
+                LeftButtonContent = "Có",
+                RightButtonContent = "Không"
+            };
+
+            //Add the dismissed event handler
+            messageBox.Dismissed += (s1, e1) =>
+            {
+                switch (e1.Result)
+                {
+                    case CustomMessageBoxResult.LeftButton:
+                         var Settings = IsolatedStorageSettings.ApplicationSettings;
+                        Settings.Remove("CheckLogin");
+                        NavigationService.Navigate(new Uri("/Pages/Login.xaml", UriKind.Relative));
+                        break;
+                    case CustomMessageBoxResult.RightButton:
+                        //add the task you wish to perform when user clicks on no button here
+
+                        break;
+                    case CustomMessageBoxResult.None:
+                        // Do something.
+                        break;
+                    default:
+                        break;
+                }
+            };
+
+            //add the show method
+            messageBox.Show();
+
+            
+        }
+
 
         private void tbl_Tap_ChangePassword(object sender, System.Windows.Input.GestureEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/Pages/DriverChangePassword.xaml", UriKind.Relative));
+            NavigationService.Navigate(new Uri("/Pages/RiderChangePassword.xaml", UriKind.Relative));
         }
 
-        private void tbn__Tap_AddWorkPlace(object sender, System.Windows.Input.GestureEventArgs e)
+        private void img_EditIcon_HomeAddress_Tap(object sender, System.Windows.Input.GestureEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/Pages/DriverAddWorkPlace.xaml", UriKind.Relative));
+            NavigationService.Navigate(new Uri("/Pages/RiderAddHomePlace.xaml", UriKind.Relative));
         }
 
-        private void tbn__Tap_AddHomePlace(object sender, System.Windows.Input.GestureEventArgs e)
+        private void img_EditIcon_OfficeAddress_Tap(object sender, System.Windows.Input.GestureEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/Pages/DriverAddHomePlace.xaml", UriKind.Relative));
+            NavigationService.Navigate(new Uri("/Pages/RiderAddWorkPlace.xaml", UriKind.Relative));
         }
+
+
+
+        
+
+        
+
+      
     }
 }
--- Source Code/FTaxi_Project_FPT_Uiversity/FT-Driver/Pages/Login.xaml.cs
+++ Source Code/FTaxi_Project_FPT_Uiversity/FT-Driver/Pages/Login.xaml.cs
@@ -13,27 +13,109 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using FT_Driver.Classes;
 using FT_Driver.Resources;
 
 namespace FT_Driver.Pages
 {
     public partial class Login : PhoneApplicationPage
     {
+        IsolatedStorageFile ISOFile = IsolatedStorageFile.GetUserStoreForApplication();
+        List<UserData> ObjUserDataList = new List<UserData>();
         public Login()
         {
             InitializeComponent();
-            
+            this.Loaded += Login_Loaded;
         }
+
+        private void Login_Loaded(object sender, RoutedEventArgs e)
+        {
+            var Settings = IsolatedStorageSettings.ApplicationSettings;
+            //Check if user already login,so we need to direclty navigate to details page instead of showing login page when user launch the app.  
+            if (Settings.Contains("CheckLogin"))
+            {
+                NavigationService.Navigate(new Uri("DriverProfile.xaml", UriKind.Relative));
+            }
+            else
+            {
+                if (ISOFile.FileExists("RegistrationDetails"))//loaded previous items into list  
+                {
+                    using (IsolatedStorageFileStream fileStream = ISOFile.OpenFile("RegistrationDetails", FileMode.Open))
+                    {
+                        DataContractSerializer serializer = new DataContractSerializer(typeof(List<UserData>));
+                        ObjUserDataList = (List<UserData>)serializer.ReadObject(fileStream);
+
+                    }
+                }
+            }
+        }
+
+        
 
         private void tbn_Tap_Login(object sender, System.Windows.Input.GestureEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/Pages/DriverProfile.xaml", UriKind.Relative));
+            NavigationService.Navigate(new Uri("/Pages/DriverProfile.xaml", UriKind.Relative)); 
+           
         }
 
        
+        private void tbl_Tap_LostPassword(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pages/DriverLostPassword.xaml", UriKind.Relative));
+        }
 
       
+
+        private void Button_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pages/HomePage.xaml", UriKind.Relative));
+        }
+
+        private void txt_Account_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txt_Account.Text == "Xin mời nhập Email tại đây")
+            {
+                txt_Account.Text = "";
+                SolidColorBrush Brush1 = new SolidColorBrush();
+                Brush1.Color = Colors.Black;
+                txt_Account.Foreground = Brush1;
+            }
+        }
+
+        private void txt_Account_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txt_Account.Text == String.Empty)
+            {
+                txt_Account.Text = "Xin mời nhập Email tại đây";
+                SolidColorBrush Brush2 = new SolidColorBrush();
+                Brush2.Color = Colors.Gray
+                    ;
+                txt_Account.Foreground = Brush2;
+            }
+        }
+
+     
+
+      
+
+
+        private void PasswordLostFocus(object sender, RoutedEventArgs e)
+        {
+            CheckPasswordWatermark();
+        }
+
+        public void CheckPasswordWatermark()
+        {
+            var passwordEmpty = string.IsNullOrEmpty(Password.Password);
+            txt_Password.Opacity = passwordEmpty ? 100 : 0;
+            Password.Opacity = passwordEmpty ? 0 : 100;
+        }
        
-        
+        private void PasswordGotFocus(object sender, RoutedEventArgs e)
+        {
+            txt_Password.Opacity = 0;
+            Password.Opacity = 100;
+        }
+
     }
 }
