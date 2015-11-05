cdb168bf72814e49d669338ed9c3a769b663ef93
apolong <apolong.lap@gmail.com>
2015-11-05 23:09:38 +0700

    Update Menu & reference
    Signed-off-by: apolong <apolong.lap@gmail.com>

---
+++ Source Code/FTaxi_Project_FPT_Uiversity/FT-Rider/Classes/GeoCoordinateConvert.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Device.Location;
+using Windows.Devices.Geolocation;
+
+namespace FT_Rider.Classes
+{
+    class GeoCoordinateConvert
+    {
+        public static GeoCoordinate ConvertGeocoordinate(Geocoordinate geocoordinate)
+        {
+            return new GeoCoordinate
+                (
+                geocoordinate.Latitude,
+                geocoordinate.Longitude,
+                geocoordinate.Altitude ?? Double.NaN,
+                geocoordinate.Accuracy,
+                geocoordinate.AltitudeAccuracy ?? Double.NaN,
+                geocoordinate.Speed ?? Double.NaN,
+                geocoordinate.Heading ?? Double.NaN
+                );
+        }
+    }
+}
