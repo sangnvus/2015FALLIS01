81fa88a0885687e4d8fc88ecb38088bf07f1ba7b
apolong <apolong.lap@gmail.com>
2015-11-12 19:47:45 +0700

    LapNV: Add Classes Signed-off-by: apolong <apolong.lap@gmail.com>

---
+++ Source Code/FTaxi_Project_FPT_Uiversity/FT-Rider/Classes/FareCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FT_Rider.Classes
+{
+    class FareCalculator
+    {
+        public static Double TaxiPriceCalculator(Double kmInput, Double priceInput)
+        {
+            Double price;
+            price = 0;
+            return price;
+        }
+    }
+}
