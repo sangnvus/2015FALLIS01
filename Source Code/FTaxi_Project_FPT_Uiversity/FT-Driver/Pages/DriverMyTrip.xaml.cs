ebc0c5d85ef6e4742e4c9bcd29810cf6f27c37a3
apolong <apolong.lap@gmail.com>
2015-12-07 18:04:01 +0700

    LapNV: 80% Driver
    Signed-off-by: apolong <apolong.lap@gmail.com>

---
+++ Source Code/FTaxi_Project_FPT_Uiversity/FT-Driver/Pages/DriverMyTrip.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace FT_Driver.Pages
+{
+    public partial class DriverMyTrip : PhoneApplicationPage
+    {
+        public DriverMyTrip()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ViewTripDetail_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+
+        }
+
+        private void btn_AlertAssets_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+
+        }
+
+        private void btn_Cancel_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+
+        }
+
+        private void btn_AddFavorite_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+
+        }
+
+        private void btn_Close_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+
+        }
+
+        private void lls_MyTrip_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
