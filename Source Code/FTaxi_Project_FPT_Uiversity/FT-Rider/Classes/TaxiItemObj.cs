0d205bd9a3bf6794ff8c5af850d1a613c2e2ffd6
apolong <apolong.lap@gmail.com>
2015-12-13 01:09:59 +0700

    LapNV: Taxi List
    Signed-off-by: apolong <apolong.lap@gmail.com>

---
+++ Source Code/FTaxi_Project_FPT_Uiversity/FT-Rider/Classes/TaxiItemObj.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FT_Rider.Classes
+{
+    class TaxiItemObj
+    {
+        private string _id;
+
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        private string _phone;
+
+        public string Phone
+        {
+            get { return _phone; }
+            set { _phone = value; }
+        }
+
+        public TaxiItemObj(string id, string name, string phone)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Phone = phone;
+        }
+
+    }
+}
