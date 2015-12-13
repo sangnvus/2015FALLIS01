ec6336700d585d2ddde89cfe45e2fbf2fd9794d8
apolong <apolong.lap@gmail.com>
2015-12-14 00:40:11 +0700

    LapNV: Complete Rider Profile
    Signed-off-by: apolong <apolong.lap@gmail.com>

--- Source Code/FTaxi_Project_FPT_Uiversity/FT-Driver/Pages/DriverProfile.xaml.cs
+++ Source Code/FTaxi_Project_FPT_Uiversity/FT-Driver/Pages/DriverProfile.xaml.cs
@@ -24,85 +24,13 @@ namespace FT_Driver.Pages
 
     {
          
-        IsolatedStorageFile ISOFile = IsolatedStorageFile.GetUserStoreForApplication();
-        UserData ObjUserData = new UserData();
 
         
         public DriverProfile()
         {
-            
-            
             InitializeComponent();
-            lpk_Language.SetValue(Microsoft.Phone.Controls.ListPicker.ItemCountThresholdProperty, 3);
-            List<Language> source = new List<Language>();
-            source.Add(new Language() { Name = "Tiếng Việt", Logo = "VI" });
-            source.Add(new Language() { Name = "English", Logo = "EN" });
-            this.lpk_Language.ItemsSource = source;
-          
-
-            this.Loaded += DriverProfile_Loaded;
         }
 
-        private void DriverProfile_Loaded(object sender, RoutedEventArgs e)
-        {
-            if (ISOFile.FileExists("CurrentLoginUserDetails"))//read current user login details    
-            {
-                using (IsolatedStorageFileStream fileStream = ISOFile.OpenFile("CurrentLoginUserDetails", FileMode.Open))
-                {
-                    DataContractSerializer serializer = new DataContractSerializer(typeof(UserData));
-                    ObjUserData = (UserData)serializer.ReadObject(fileStream);
-
-                }
-                StckUserDetailsUI.DataContext = ObjUserData;  
-            }  
-        }
-
-
-        
-
-        private void btn__Tap_Logout(object sender, System.Windows.Input.GestureEventArgs e)
-        {
-            LogOut();
-        }
-
-        public void LogOut()
-        {
-            CustomMessageBox messageBox = new CustomMessageBox()
-            {
-                //set the properties
-                Caption = "Thông báo",
-                Message = "Bạn có muốn thoát không?",
-                LeftButtonContent = "Có",
-                RightButtonContent = "Không"
-            };
-
-            //Add the dismissed event handler
-            messageBox.Dismissed += (s1, e1) =>
-            {
-                switch (e1.Result)
-                {
-                    case CustomMessageBoxResult.LeftButton:
-                         var Settings = IsolatedStorageSettings.ApplicationSettings;
-                        Settings.Remove("CheckLogin");
-                        NavigationService.Navigate(new Uri("/Pages/Login.xaml", UriKind.Relative));
-                        break;
-                    case CustomMessageBoxResult.RightButton:
-                        //add the task you wish to perform when user clicks on no button here
-
-                        break;
-                    case CustomMessageBoxResult.None:
-                        // Do something.
-                        break;
-                    default:
-                        break;
-                }
-            };
-
-            //add the show method
-            messageBox.Show();
-
-            
-        }
 
 
         private void tbl_Tap_ChangePassword(object sender, System.Windows.Input.GestureEventArgs e)
@@ -119,13 +47,6 @@ namespace FT_Driver.Pages
         {
             NavigationService.Navigate(new Uri("/Pages/CompanyInfo.xaml", UriKind.Relative));
         }
-
-
-
-        
-
-        
-
       
     }
 }
